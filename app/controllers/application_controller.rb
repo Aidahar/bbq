dd705726e5e207cd7cbc85e4fef151d4c323c362
Aidahar <zagirik@gmail.com>
2019-05-05 13:27:02 +0300

    edit author permission & anonym permission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  helper_method :current_user_can_edit?
+  helper_method :current_user_can_edit?, :current_user_subscribed?, :subscriptions_empty?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update,
@@ -12,5 +12,13 @@ class ApplicationController < ActionController::Base
   def current_user_can_edit?(model)
     user_signed_in? &&
     (model.user == current_user || (model.try(:event).present? && model.event.user == current_user))
+  end
+
+  def current_user_subscribed?(event)
+    event.subscribers.include?(current_user)
+  end
+
+  def subscriptions_empty?(event)
+    Subscription.where(event_id: event.id).empty?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   before_validation :set_name, on: :create
 
   validates :name, presence: true, length: {maximum: 35}
+  validate :check_mail, unless: -> { user.present? }
 
   after_commit :link_subscriptions, on: :create
 
@@ -21,5 +22,9 @@ class User < ApplicationRecord
 
   def link_subscriptions
     Subscription.where(user_id: nil, user_email: user.email).update_all(user_id: self.id)
+  end
+
+  def check_mail
+    errors.add(:user_email, :not_available) if User.find_by(email: user_email)
   end
 end
