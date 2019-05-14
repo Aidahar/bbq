4ff0471c1c070a72ab490dad9d97b91e556f5a5b
Aidahar <zagirik@gmail.com>
2019-05-14 21:45:35 +0300

    event-mailer, sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,8 +33,6 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
-  config.action_mailer.perform_caching = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -59,15 +57,14 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000'}
-
+  config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-
   config.action_mailer.smtp_settings = {
-    address: 'smtp.yandex.ru',
-    port: '587',
-    user_name: 'andreytestopolis', # не используйте для тестов свои реальные ящики
-    password: 'Cfknfyfn1981',
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+    :address              => "smtp.yandex.ru",
+        :port                 => 587,
+        :user_name            => "",
+        :password             => "",
+        :authentication       => "plain",
+        :enable_starttls_auto => true
+  } 
 end
